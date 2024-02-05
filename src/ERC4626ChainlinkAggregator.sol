be62155763a7920ef278e2469b9f9aaf131db8cc
Sebastien <sderivaux@gmail.com>
2024-02-05 20:21:01 +0100

    recommit

--- src/ERC4626ChainlinkAggregator.sol
+++ src/ERC4626ChainlinkAggregator.sol
@@ -1,9 +1,11 @@
 // SPDX-License-Identifier: GPL-2.0-or-later
 pragma solidity ^0.8.0;
 
-import {IERC4626} from "@openzeppelin/contracts/interfaces/IERC4626.sol";
+import {IERC4626, ChainlinkOracle} from "morpho-blue-oracles/ChainlinkOracle.sol";
 
-contract ERC4626ChainlinkAggregator {
+import {AggregatorV3Interface, ChainlinkDataFeedLib} from "morpho-blue-oracles/libraries/ChainlinkDataFeedLib.sol";
+
+contract ERC4626ChainlinkAggregator is AggregatorV3Interface {
     IERC4626 immutable public token;
 
     constructor(IERC4626 token_) {
@@ -11,10 +13,27 @@ contract ERC4626ChainlinkAggregator {
     }
 
     function latestRoundData() external view returns (uint80, int256, uint256, uint256, uint80) {
-        return (0, int256(token.convertToAssets(10**8)), 0, 0, 0);
+        return (0, int256(token.convertToAssets(10**18)), 0, 0, 0);
     }
 
-    function decimals() external pure returns (uint256) {
-        return 8;
+    function decimals() external pure returns (uint8) {
+        return 18;
+    }
+
+    
+    function description() external view returns (string memory) {
+        return "ERC4626ChainlinkAggregator";
+    }
+
+    function version() external view returns (uint256) {
+        return 1;
+    }
+
+    function getRoundData(uint80 _roundId)
+        external
+        view
+        returns (uint80 roundId, int256 answer, uint256 startedAt, uint256 updatedAt, uint80 answeredInRound) {
+        require(_roundId == 0, "Round ID should be 0");
+        return (0, int256(token.convertToAssets(10**18)), 0, 0, 0);
     }
 }
--- test/ERC4626ChainlinkAggregator.t.sol
+++ test/ERC4626ChainlinkAggregator.t.sol
@@ -5,33 +5,59 @@ import "../lib/forge-std/src/Test.sol";
 import "../lib/forge-std/src/console.sol";
 
 
-import {IERC20} from "@openzeppelin/contracts/interfaces/IERC20.sol";
-import {IERC4626} from "@openzeppelin/contracts/interfaces/IERC4626.sol";
+
+import {IERC4626, ChainlinkOracle} from "morpho-blue-oracles/ChainlinkOracle.sol";
+
+import {AggregatorV3Interface, ChainlinkDataFeedLib} from "morpho-blue-oracles/libraries/ChainlinkDataFeedLib.sol";
+import {VaultLib} from "morpho-blue-oracles/libraries/VaultLib.sol";
 
 import "../src/ERC4626ChainlinkAggregator.sol";
 
-
 contract ERC4626ChainlinkAggregatorTest is Test {
 
-    IERC20 constant dai = IERC20(0x6B175474E89094C44Da98b954EedeAC495271d0F);
+
+    AggregatorV3Interface constant feedZero = AggregatorV3Interface(address(0));
+
+    AggregatorV3Interface constant ethUsdFeed = AggregatorV3Interface(0x5f4eC3Df9cbd43714FE2740f5E3616155c5b8419);
+
+    IERC4626 constant vaultZero = IERC4626(address(0));
+
     IERC4626 constant sdai = IERC4626(0x83F20F44975D03b1b09e64809B757c47f942BEeA);
 
     IERC4626 constant wusdm = IERC4626(0x57F5E098CaD7A3D1Eed53991D4d66C45C9AF7812);
-
-    ERC4626ChainlinkAggregator public oracle;
 
     function setUp() public {
         vm.createSelectFork(vm.envString("ETH_RPC_URL"));
     }
 
     
-    function testWusdmOracle() public {
-        
-        oracle = new ERC4626ChainlinkAggregator(wusdm);
+    function testWusdmAdapter() public {
+        ERC4626ChainlinkAggregator oracle = new ERC4626ChainlinkAggregator(wusdm);
         (, int256 price, , , ) = oracle.latestRoundData();
 
         console.logInt(price);
-        assertEq(uint256(price), wusdm.convertToAssets(10**8));
+        assertEq(uint256(price), wusdm.convertToAssets(10**18));
+    }
+
+    
+    function testWusdmOracle() public {
+        ERC4626ChainlinkAggregator wusdmOracle = new ERC4626ChainlinkAggregator(wusdm);
+
+        ChainlinkOracle oracle = new ChainlinkOracle(vaultZero, ethUsdFeed, feedZero, wusdmOracle, feedZero, 1, 18, 18);
+        int256 quoteAnswer = int256(wusdm.convertToAssets(10**18));
+        console.logInt(quoteAnswer);
+
+        (, int256 baseAnswer,,,) = ethUsdFeed.latestRoundData();
+        console.logInt(baseAnswer);
+        console.logUint( oracle.price());
+
+        
+           
+        assertEq(
+            oracle.price(),
+            (uint256(baseAnswer) * 10 ** (36 + 18 + 18 - 8 - 18)) // Not exactly sure why
+                / uint256(quoteAnswer)
+        );
     }
 
 
